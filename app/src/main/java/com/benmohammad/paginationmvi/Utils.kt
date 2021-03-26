fb04e62bdefb8847a0a746826aaec30932d33f5f
Ben Mohammad <benmohammad593@gmail.com>
2021-03-26 06:54:10 +0000

    Initial commit

---
+++ app/src/main/java/com/benmohammad/paginationmvi/Utils.kt
@@ -0,0 +1,26 @@
+package com.benmohammad.paginationmvi
+
+import android.content.Context
+import android.content.res.Configuration
+import android.widget.Toast
+import androidx.annotation.CheckResult
+import androidx.fragment.app.Fragment
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Observable
+import io.reactivex.subjects.Subject
+
+@CheckResult
+inline fun <T : Any, R : Any> Observable<T>.exhaustive(crossinline transform: (T) -> Observable<out R>): Observable<R> {
+    return this
+        .toFlowable(BackpressureStrategy.DROP)
+        .flatMap({transform(it).toFlowable(BackpressureStrategy.MISSING)}, 1)
+        .toObservable()
+}
+
+inline fun <T> Subject<T>.asObservable(): Observable<T> = this
+
+val Context.isOrientationPortrait get() = this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+
+fun Context.toast(text: CharSequence) = Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+fun Fragment.toast(text: CharSequence) = requireContext().toast(text)
